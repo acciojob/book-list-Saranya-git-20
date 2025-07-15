fe49abcc2277181edb8f5894d1f4790d21489542
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-15 22:34:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
  function addvalue() {
-	const title = document.getElementById("title").value;
+    const title = document.getElementById("title").value;
     const author = document.getElementById("author").value;
     const isbn = document.getElementById("isbn").value;
 
-	const table = document.getElementById("book-list")
-	const.newrow = table.insertRow();
-	const.cell1 = newrow.inserCell();
-	const.cell2 = newrow.inserCell();
-	const.cell3 = newrow.insertCell();
-	const.cell4 = newrow.inserCell();
+    const table = document.getElementById("book-list");
+    const newRow = table.insertRow();
 
-	cell1.innerHTML=title;
-	cell2.innerHTML = author;
-	cell3.innerHTML = isbn;
-	cell4.innerHTML = `<span class="delete-btn" onclick="deleteRow(this)">&times;</span>`
-	 function deleteRow(btn) {
+    const cell1 = newRow.insertCell();
+    const cell2 = newRow.insertCell();
+    const cell3 = newRow.insertCell();
+    const cell4 = newRow.insertCell();
+
+    cell1.innerHTML = title;
+    cell2.innerHTML = author;
+    cell3.innerHTML = isbn;
+    cell4.innerHTML = `<span class="delete-btn" onclick="deleteRow(this)">&times;</span>`;
+
+  function deleteRow(btn) {
     const row = btn.closest("tr");
     row.remove();
   }
-	
-}
