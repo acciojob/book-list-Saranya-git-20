2522eaf2a5fdf6e6d8e62ce9ecb4d530f72d368d
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-15 22:25:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+ function addvalue() {
+	const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const isbn = document.getElementById("isbn").value;
+
+	const table = document.getElementById("book-list")
+	const.newrow = table.insertRow();
+	const.cell1 = newrow.inserCell();
+	const.cell2 = newrow.inserCell();
+	const.cell3 = newrow.insertCell();
+	const.cell4 = newrow.inserCell();
+
+	cell1.innerHTML=title;
+	cell2.innerHTML = author;
+	cell3.innerHTML = isbn;
+	cell4.innerHTML = `<span class="delete-btn" onclick="deleteRow(this)">&times;</span>`
+	 function deleteRow(btn) {
+    const row = btn.closest("tr");
+    row.remove();
+  }
+	
+}
